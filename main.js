e99c0a7d11e9324c4b81776b602a956948134485
Ayellet Tsachi <ayelletts@gmail.com>
2022-04-05 19:34:13 +0300

    Change shuffle

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const cards = ["A", "B", "C", "A", "B", "C"];
 init();
 function init() {
-  shuffle(cards);
+  //shuffle cards
+  cards.sort((a, b) => 0.5 - Math.random());
   const board = document.getElementById("board");
   for (ind in cards) {
     const cardElement = createCard(ind);
@@ -10,29 +11,9 @@ function init() {
 }
 
 function createCard(ind) {
-  const element = document.createElement("span");
+  const element = document.createElement("div");
   element.innerHTML = cards[ind];
   element.id = ind;
   element.classList.add("card");
   return element;
-}
-
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
 }
