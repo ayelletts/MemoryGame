d75e92c694550ea60654203aa52edfd0c281c0fb
Ayellet Tsachi <ayelletts@gmail.com>
2022-04-10 13:19:35 +0300

    "I",   "J",   "K"

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-const cards = ["A", "B", "C", "A", "B", "C"];
+const cards = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+];
 init();
 function init() {
   //shuffle cards
@@ -10,10 +33,21 @@ function init() {
   }
 }
 
+let flipCard = function () {
+  if (this.innerHTML == "") {
+    let ind = Number(this.id);
+    this.innerHTML = cards[ind];
+  } else {
+    this.innerHTML = "";
+  }
+};
+
 function createCard(ind) {
   const element = document.createElement("div");
   element.innerHTML = cards[ind];
   element.id = ind;
+  // debugger;
+  // element.addEventListener("onclick", flipCard);
   element.classList.add("card");
   return element;
 }
